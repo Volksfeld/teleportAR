431c603534625c45b00caf4f4aae2359a735a9ec
Volksfeld <jc.mussi99@gmail.com>
2019-10-20 10:47:32 -0300

    feat(TeleportAR): Final prototype version

--- README.md
+++ README.md
@@ -6,3 +6,4 @@
 
 teleportAR is a app that allow users to teleport anywhere based on the locations provided by Nasa Earth. 
 We use viro-react to make the portals, using it's amazing AR integration. 
+ 
--- src/ARScenes/PortalScene.js
+++ src/ARScenes/PortalScene.js
@@ -21,24 +21,14 @@ export default class PortalScene extends Component {
       <ViroARScene>
         <ViroAmbientLight color="#ffffff" intensity={200}/>
         <ViroPortalScene passable={true} dragType="FixedDistance" onDrag={()=>{}}>
-          <ViroPortal position={[0, 0, -1]} scale={[.1, .1, .1]}>
-            <Viro3DObject source={require('./assets/portal_ship/portal_ship.vrx')}
-              resources={[require('./assets/portal_ship/portal_ship_diffuse.png'),
-                          require('./assets/portal_ship/portal_ship_normal.png'),
-                          require('./assets/portal_ship/portal_ship_specular.png')]}
+          <ViroPortal position={[0, -0.28, -4.75]} scale={[1.80, 1.1, 0.30]}>
+            <Viro3DObject source={require('./assets/portal_wood_frame/portal_wood_frame.vrx')}
+              resources={[require('./assets/portal_wood_frame/portal_wood_frame_diffuse.png'),
+                          require('./assets/portal_wood_frame/portal_wood_frame_normal.png'),
+                          require('./assets/portal_wood_frame/portal_wood_frame_specular.png')]}
               type="VRX"/>
           </ViroPortal>
         <Viro360Image source={require("./assets/forest.jpg")} />
-        </ViroPortalScene>
-        <ViroPortalScene passable={true} dragType="FixedDistance" onDrag={()=>{}}>
-          <ViroPortal position={[1, 0, -1]} scale={[0.15, 0.15, 0.15]}>
-            <Viro3DObject source={require('./assets/portal_ship/portal_ship.vrx')}
-              resources={[require('./assets/portal_ship/portal_ship_diffuse.png'),
-                          require('./assets/portal_ship/portal_ship_normal.png'),
-                          require('./assets/portal_ship/portal_ship_specular.png')]}
-              type="VRX"/>
-          </ViroPortal>
-          <Viro360Image source={require("./assets/360_island.jpg")} />
         </ViroPortalScene>
       </ViroARScene>
     );
